a339c60c0dd39dce1f6e9d0f8f6412e5cd50f487
root <t.egbunike@alustudent.com>
2024-03-29 07:25:06 -0700

    yes

--- data.py
+++ data.py
@@ -7,5 +7,5 @@ parameters = {
 
 response = requests.get("https://opentdb.com/api.php", params=parameters)
 response.raise_for_status()
-data = response.json()
+ = response.json()
 question_data = data["results"]
